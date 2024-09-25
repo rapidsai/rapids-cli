4bd30cedd2bc1eb04a4c1e9dcc4c46f7eaabfd27
Melody Wang <melodyw@nvidia.com>
2024-09-25 15:16:20 -0400

    added glb check

--- rapids_cli.py
+++ rapids_cli.py
@@ -105,12 +105,14 @@ def detect_os():
     system = platform.system()
     release = platform.release()
     version = platform.version()
+    os = ""
 
     print(f"        System: {system}")
     print(f"        Release: {release}")
     print(f"        Version: {version}")
     validOS = False 
     if system == "Windows":
+        os = "Windows"
         if release == '11': 
             try:
                 result = subprocess.check_output(['wsl', '--list', '--verbose'], text=True)
@@ -127,16 +129,22 @@ def detect_os():
         try: 
             with open('/etc/os-release') as f: 
                 os_release = f.read()
+                os = get_os_attributes(os_release)["NAME"]
                 os_attributes =get_os_attributes(os_release)
                 validOS = check_os_version(os_attributes)
         except FileNotFoundError:
             print("/etc/os-release file not found. This might not be a typical Linux environment.")
     else:
         print(f"      {X_MARK} Operating System not recognized")
+        os = None
+
+        
     if validOS: 
         print(f"      {OK_MARK} OS is compatible with RAPIDS")
     else:
         print(f"      {X_MARK} OS is not compatible with RAPIDS. Please see https://docs.rapids.ai/install for system requirements.")
+
+    return os
 
 #CUDA Version : NVIDIA DRIVER Version
 SUPPORTED_VERSIONS = {
@@ -308,7 +316,34 @@ def check_conda(conda_requirement):
         print(f"      {X_MARK} CONDA Version is not compatible with RAPIDS - please upgrade to Docker {conda_requirement}")
     
 
-#def check_pip():
+
+
+def check_glb():
+    print(f"   {CHECK_SYMBOL} Checking for [italic red]glb comp[/italic red]")
+    glb_compatible = False
+    result = subprocess.check_output(["ldd", "--version"])
+    glb_version = result.decode('utf-8').strip().split("\n")[0].split(" ")[-1]
+    
+    machine = platform.machine()
+
+    if machine == 'x86_64':
+        if glb_version >= "2.17":
+            glb_compatible = True 
+    elif machine == 'aarch64' or machine == "arm64":
+        if glb_version >= "2.32":
+            glb_compatible =  True 
+    else: 
+        print(f"      {X_MARK} Please only use x86_64 or arm64 architectures")
+    if glb_compatible:
+        print(f"      {OK_MARK} GLB version and CPU architecture are compatible with each other")
+    else:
+        print(f"      {X_MARK} GLB version and CPU architecture are NOT compatible with each other. ")
+        
+        if machine == 'x86_64':
+            print(f"      Please upgrade glb to 2.17 and above")
+        elif machine == 'aarch64' or machine == "arm64":
+            print(f"      Please upgrade glb to 2.32 and above")
+
 
 
 @click.group()
@@ -382,7 +417,7 @@ def doctor():
         check_gpu_compute_capability("70")
     if cuda_check_return:
         check_driver_compatibility()
-    detect_os()
+    os = detect_os()
 
     print("\n")
     print(f"[bold green]{DOCTOR_SYMBOL} Performing RECOMMENDED health check for RAPIDS[/bold green] \n")
@@ -395,6 +430,11 @@ def doctor():
     print(f"[bold green]{DOCTOR_SYMBOL} Performing OTHER health checks for RAPIDS[/bold green] \n")
     check_docker("19.03")
     check_conda("22.11")
+
+    if os == "Ubuntu":
+        check_glb()
+
+
 
 @rapids.command()
 def info():
