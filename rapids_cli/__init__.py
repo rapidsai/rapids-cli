43a5a5d7f77bde1f1df595ee5ea910df0b7d5d5d
Melody Wang <melodyw@nvidia.com>
2024-10-14 11:58:06 -0400

    reorganized cudf checks

--- rapids_cli/__init__.py
+++ rapids_cli/__init__.py
@@ -10,25 +10,14 @@ from rich.console import Console
 from rich.table import Table
 import platform
 
-from rapids_cli.os_checks import detect_os
-from rapids_cli.gpu_checks.gpu_memory_checks import check_memory_to_gpu_ratio
-from rapids_cli.gpu_checks.nvlink import check_nvlink_status
-from rapids_cli.gpu_checks import gpu_check, check_gpu_compute_capability
-from rapids_cli.cuda_driver_checks import get_cuda_version, cuda_check, get_driver_version, check_driver_compatibility
-from rapids_cli.cuda_driver_checks.sdd_nvme import check_sdd_nvme
-from rapids_cli.dependency_checks import check_conda, check_pip,check_docker, check_glb
-
-CHECK_SYMBOL = "🚨"
-OK_MARK = "✅"
-X_MARK = "❌"
-DOCTOR_SYMBOL = "🧑‍⚕️"
-VALID_SUBCOMMANDS = ["cudf"]
+from rapids_cli.default_checks import default_checks
+from rapids_cli.cudf_checks import cudf_checks
 
 
-def compare_version(version, requirement):
-    if str(version) >= str(requirement): 
-        return True
-    return False 
+with open('config.yml', 'r') as file: 
+    config = yaml.safe_load(file)
+
+VALID_SUBCOMMANDS = config['valid_subcommands']['VALID_SUBCOMMANDS']
 
 
 @click.group()
@@ -91,74 +80,8 @@ def help():
     console = Console()
     console.print(table)
 
-
-def cudf_checks(cuda_requirement, driver_requirement, compute_requirement):
-
-    print(f"[bold green] {DOCTOR_SYMBOL} Performing REQUIRED health check for CUDF [/bold green] \n")
-    
-    
-    print(f"   {CHECK_SYMBOL} Checking for [italic red]CUDA dependencies[/italic red]")
-    if compare_version(get_cuda_version(), cuda_requirement): #when the other branch gets merged, will move the magic numbers to their yaml file 
-        print(f"{OK_MARK: >6}  CUDA version compatible with CUDF")
-    else:
-        print(f"{X_MARK: >6}  CUDA version not compatible with CUDF. Please upgrade to {cuda_requirement}")
-    
-
-    print(f"   {CHECK_SYMBOL} Checking for [italic red]Driver Availability[/italic red]")
-    if cuda_check():
-        if compare_version(get_driver_version(), driver_requirement):
-            print(f"{OK_MARK: >6}  Nvidia Driver version compatible with CUDF")
-        else:
-            print(f"{X_MARK: >6}  Nvidia Driver version not compatible with CUDF. Please upgrade to {driver_requirement}")
-    else: 
-        print(f"{X_MARK: >6} No Nvidia Driver Detected")
-
-    if gpu_check():
-        if check_gpu_compute_capability(compute_requirement):
-            print(f"{OK_MARK: >6}  GPU compute compatible with CUDF")
-        else:
-            print(f"{X_MARK: >6}  GPU compute not compatible with CUDF. Please upgrade to compute >={compute_requirement}") 
    
-    
-def default_checks(): 
-    print(f"[bold green] {DOCTOR_SYMBOL} Performing REQUIRED health check for RAPIDS [/bold green] \n")
 
-
-    
-    with open('config.yml', 'r') as file: 
-        config = yaml.safe_load(file)
-    
-
-    gpu_compute_requirement = config['min_supported_versions']['gpu_compute_requirement']
-    docker_requirement = config['min_supported_versions']['docker_requirement']
-    conda_requirement = config['min_supported_versions']['conda_requirement']
-
-
-    gpu_check_return = gpu_check()
-    cuda_check_return = cuda_check()
-    if gpu_check_return:
-        check_gpu_compute_capability(gpu_compute_requirement)
-    if cuda_check_return:
-        check_driver_compatibility()
-    os = detect_os()
-
-    print("\n")
-    print(f"[bold green]{DOCTOR_SYMBOL} Performing RECOMMENDED health check for RAPIDS[/bold green] \n")
-    check_sdd_nvme()
-    if gpu_check_return:
-        check_memory_to_gpu_ratio()
-        check_nvlink_status()
-
-    print("\n")
-    print(f"[bold green]{DOCTOR_SYMBOL} Performing OTHER health checks for RAPIDS[/bold green] \n")
-    check_docker(docker_requirement)
-    check_conda(conda_requirement)
-
-    if cuda_check_return:
-        check_pip()
-    
-    if os == 'Ubuntu':
-        check_glb()
 
 
 @rapids.command()
