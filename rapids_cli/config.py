15b2855bd9c8308359e08e03453bf5bd21a72bb6
Jacob Tomlinson <jtomlinson@nvidia.com>
2024-10-16 14:12:40 +0100

    Rename variable to be more correct

--- rapids_cli/config.py
+++ rapids_cli/config.py
@@ -2,8 +2,8 @@ import pathlib
 import yaml
 
 
-_CURRENT_DIR = pathlib.Path(__file__).parent
+_ROOT_DIR = pathlib.Path(__file__).parent
 
 
-with open(_CURRENT_DIR / 'config.yml', 'r') as file: 
+with open(_ROOT_DIR / 'config.yml', 'r') as file: 
     config = yaml.safe_load(file)
