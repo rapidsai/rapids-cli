51fd50333d7ce0273ae42f48a365bf8ff05a59f9
Melody Wang <98235366+melodywang060@users.noreply.github.com>
2024-10-24 04:56:07 -0400

    added precommit and fixed linting errors (#48)
    Address issue #38 
    
    Enabled the automatic linting tests for the following tools:
    
    ```
    black-jupyter............................................................Passed 
    blacken-docs.............................................................Passed 
    prettier.................................................................Passed 
    markdownlint.............................................................Passed 
    ruff.....................................................................Passed 
    shellcheck...........................................(no files to check)Skipped
    ```

--- rapids_cli/config.py
+++ rapids_cli/config.py
@@ -5,5 +5,5 @@ import yaml
 _ROOT_DIR = pathlib.Path(__file__).parent
 
 
-with open(_ROOT_DIR / 'config.yml', 'r') as file: 
+with open(_ROOT_DIR / "config.yml", "r") as file:
     config = yaml.safe_load(file)
--- rapids_cli/doctor/checks/memory.py
+++ rapids_cli/doctor/checks/memory.py
@@ -1,19 +1,19 @@
-import psutil 
+import psutil
 import pynvml
 
-from rapids_cli.constants import CHECK_SYMBOL, OK_MARK, X_MARK, DOCTOR_SYMBOL
+from rapids_cli.constants import CHECK_SYMBOL, OK_MARK, X_MARK
 
 
 def get_system_memory():
     virtual_memory = psutil.virtual_memory()
-    total_memory = virtual_memory.total / (1024 ** 3) #converts bytes to gigabytes
+    total_memory = virtual_memory.total / (1024**3)  # converts bytes to gigabytes
     print("System Memory Information: \n")
     print(f"Total Virtual Memory: {virtual_memory.total / (1024 ** 3):.2f} GB")
     print(f"Available Virtual Memory: {virtual_memory.available / (1024 ** 3):.2f} GB")
     print(f"Used Virtual Memory: {virtual_memory.used / (1024 ** 3):.2f} GB")
     return total_memory
-    
-    
+
+
 def get_gpu_memory():
     pynvml.nvmlInit()
     gpus = pynvml.nvmlDeviceGetCount()
@@ -21,7 +21,7 @@ def get_gpu_memory():
     for i in range(gpus):
         handle = pynvml.nvmlDeviceGetHandleByIndex(i)
         memory_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
-        gpu_memory_total += memory_info.total / (1024 ** 3) #converts to gigabytes
+        gpu_memory_total += memory_info.total / (1024**3)  # converts to gigabytes
         print(f"GPU {i} memory: {memory_info.total / (1024 ** 3):.2f} GB")
 
     pynvml.nvmlShutdown()
@@ -30,22 +30,28 @@ def get_gpu_memory():
     return gpu_memory_total
 
 
-#checks that approximately 2:1 ratio of system Memory to total GPU Memory (especially useful for Dask)
+# checks that approximately 2:1 ratio of system Memory to total GPU Memory (especially useful for Dask)
 def check_memory_to_gpu_ratio():
-    print(f"   {CHECK_SYMBOL} Checking for approximately [italic red]2:1 system Memory to total GPU memory ratio[/italic red]")
+    print(
+        f"   {CHECK_SYMBOL} Checking for approximately [italic red]2:1 system Memory to total GPU memory ratio[/italic red]"
+    )
     try:
         pynvml.nvmlInit()
         system_memory = get_system_memory()
         gpu_memory = get_gpu_memory()
         ratio = system_memory / gpu_memory
         print(f"      System Memory to GPU Memory Ratio: {ratio:.2f}")
-        if ratio >= 1.8 and ratio <=2.2:
-            print(f"{OK_MARK: >6} Approximately 2:1 ratio of system Memory to total GPU Memory (especially useful for Dask).")
+        if ratio >= 1.8 and ratio <= 2.2:
+            print(
+                f"{OK_MARK: >6} Approximately 2:1 ratio of system Memory to total GPU Memory (especially useful for Dask)."
+            )
         else:
-            print(f"{X_MARK: >6} System Memory to total GPU Memory ratio not approximately 2:1 ratio.")
-        
+            print(
+                f"{X_MARK: >6} System Memory to total GPU Memory ratio not approximately 2:1 ratio."
+            )
+
         pynvml.nvmlShutdown()
-        return True 
-    except: 
+        return True
+    except pynvml.NVMLError:
         print(f"{X_MARK: >6} GPU not found. Please ensure GPUs are installed.")
         return False
