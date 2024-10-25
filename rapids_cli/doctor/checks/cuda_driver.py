fcd7531f54183952472dd2d6d5c93352e0efb6df
Melody Wang <melodyw@nvidia.com>
2024-10-25 14:33:19 -0400

    added mypy checks

--- rapids_cli/doctor/checks/cuda_driver.py
+++ rapids_cli/doctor/checks/cuda_driver.py
@@ -37,6 +37,7 @@ SUPPORTED_VERSIONS = {
 
 
 def get_cuda_version():
+
     try:
         output = subprocess.check_output(["nvcc", "--version"])
         # print(output)
@@ -50,6 +51,7 @@ def get_cuda_version():
 
 
 def get_driver_version():
+
     try:
         result = subprocess.run(
             ["nvidia-smi", "--query-gpu=driver_version", "--format=csv,noheader"],
