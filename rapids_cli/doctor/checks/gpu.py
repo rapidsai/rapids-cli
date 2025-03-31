72a0f359ef2cd16c602bcc9d1ce5c91541b824ed
Gil Forsyth <gforsyth@nvidia.com>
2025-03-31 17:07:58 -0400

    refactor: restrict try-except blocks to lines that raise exceptions

--- rapids_cli/doctor/checks/gpu.py
+++ rapids_cli/doctor/checks/gpu.py
@@ -10,32 +10,31 @@ def gpu_check(verbose=False):
     """Check GPU availability."""
     try:
         pynvml.nvmlInit()
-        num_gpus = pynvml.nvmlDeviceGetCount()
-        return f"GPU(s) detected: {num_gpus}"
     except pynvml.NVMLError as e:
         raise ValueError("No available GPUs detected") from e
+    num_gpus = pynvml.nvmlDeviceGetCount()
+    return f"GPU(s) detected: {num_gpus}"
 
 
 def check_gpu_compute_capability(verbose):
     """Check the system for GPU Compute Capability."""
     try:
-        required_capability = 7
         pynvml.nvmlInit()
-
-        num_gpus = pynvml.nvmlDeviceGetCount()
-        for i in range(num_gpus):
-            handle = pynvml.nvmlDeviceGetHandleByIndex(i)
-            major, minor = pynvml.nvmlDeviceGetCudaComputeCapability(handle)
-            compute_capability = major * 10 + minor
-
-            if compute_capability >= required_capability:
-                continue
-            else:
-                raise ValueError(
-                    f"GPU {i} does not meet the required compute "
-                    f"capability {required_capability[0]}.{required_capability[1]}."
-                )
-        return True
-
     except pynvml.NVMLError as e:
         raise ValueError("No GPU - cannot determineg GPU Compute Capability") from e
+
+    required_capability = 7
+    num_gpus = pynvml.nvmlDeviceGetCount()
+    for i in range(num_gpus):
+        handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+        major, minor = pynvml.nvmlDeviceGetCudaComputeCapability(handle)
+        compute_capability = major * 10 + minor
+
+        if compute_capability >= required_capability:
+            continue
+        else:
+            raise ValueError(
+                f"GPU {i} does not meet the required compute "
+                f"capability {required_capability[0]}.{required_capability[1]}."
+            )
+    return True
--- rapids_cli/doctor/checks/nvlink.py
+++ rapids_cli/doctor/checks/nvlink.py
@@ -10,17 +10,18 @@ def check_nvlink_status(verbose=True):
     """Check the system for NVLink with 2 or more GPUs."""
     try:
         pynvml.nvmlInit()
-        device_count = pynvml.nvmlDeviceGetCount()
-        if device_count < 2:
-            return False
-
-        for i in range(device_count):
-            handle = pynvml.nvmlDeviceGetHandleByIndex(i)
-            for nvlink_id in range(pynvml.NVML_NVLINK_MAX_LINKS):
-                try:
-                    pynvml.nvmlDeviceGetNvLinkState(handle, 0)
-                    return True
-                except pynvml.NVMLError as e:
-                    raise ValueError(f"NVLink {nvlink_id} Status Check Failed") from e
     except pynvml.NVMLError as e:
         raise ValueError("GPU not found. Please ensure GPUs are installed.") from e
+
+    device_count = pynvml.nvmlDeviceGetCount()
+    if device_count < 2:
+        return False
+
+    for i in range(device_count):
+        handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+        for nvlink_id in range(pynvml.NVML_NVLINK_MAX_LINKS):
+            try:
+                pynvml.nvmlDeviceGetNvLinkState(handle, 0)
+                return True
+            except pynvml.NVMLError as e:
+                raise ValueError(f"NVLink {nvlink_id} Status Check Failed") from e
