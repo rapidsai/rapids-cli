0bbb67d61799c1779f986b0b9f6bd78b5cb41849
James Lamb <jlamb@nvidia.com>
2025-05-20 23:06:11 -0500

    fix artifact downloads

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # nx-cugraph is a pure wheel, which is part of generating the download path
-WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="rapids_cli" RAPIDS_PY_WHEEL_PURE="1" rapids-download-wheels-from-github python)
+WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="rapids-cli" RAPIDS_PY_WHEEL_PURE="1" rapids-download-wheels-from-github python)
 
 # echo to expand wildcard before adding `[extra]` requires for pip
 rapids-pip-retry install \
-    $(echo "${WHEELHOUSE}/rapids_cli"*.whl)[test]
+    "$(echo "${WHEELHOUSE}"/rapids_cli*.whl)[test]"
 
 python -m pytest
