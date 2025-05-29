4a846b9c8b0b8c4992bba5efab05cd7d43e90994
James Lamb <jaylamb20@gmail.com>
2025-05-29 09:33:15 -0500

    build and test conda packages in CI (#99)
    Closes #89
    
    * adds conda CI (for PRs and branch builds)
    * uses `rattler-build` for builds, matching the direction RAPIDS is moving (https://github.com/rapidsai/build-planning/issues/47)
    * move version into a root-level `VERSION` file (so it can be referenced in both `pyproject.toml` and the `rattler-build` recipe)
    * bumps version to `0.1.1` (so we'll see packages get published)
    * introduces `rapids-dependency-file-generator`
      - *to start, just to avoid duplicating testing dependencies into `test_python.sh`*
      - *and for consistency with other RAPIDS projects*
      - *if we agree on this, I'll go further in a follow-up PR and use it to automatically manage `pyproject.toml`*
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Naty Clementi (https://github.com/ncclementi)
    
    URL: https://github.com/rapidsai/rapids-cli/pull/99

---
+++ ci/build_python.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright (c) 2022-2025, NVIDIA CORPORATION.
+
+set -euo pipefail
+
+source rapids-date-string
+
+rapids-print-env
+
+RAPIDS_PACKAGE_VERSION=$(head -1 ./VERSION)
+export RAPIDS_PACKAGE_VERSION
+
+# populates `RATTLER_CHANNELS` array and `RATTLER_ARGS` array
+source rapids-rattler-channel-string
+
+rapids-logger "Building rapids-cli"
+
+rattler-build build                   \
+    --recipe conda/recipes/rapids-cli \
+    "${RATTLER_ARGS[@]}"              \
+    "${RATTLER_CHANNELS[@]}"
+
+# remove build_cache directory to avoid uploading the entire source tree
+# tracked in https://github.com/prefix-dev/rattler-build/issues/1424
+rm -rf "$RAPIDS_CONDA_BLD_OUTPUT_DIR"/build_cache
