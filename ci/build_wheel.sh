f44456bc8d91b363b840770866e2767d3831cbcf
James Lamb <jlamb@nvidia.com>
2026-01-28 16:22:30 -0600

    enforce PEP 639 metadata, use rapids copyright hook, validate wheels

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Copyright (c) 2024-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION.
+# SPDX-License-Identifier: Apache-2.0
 
 set -euo pipefail
 
@@ -9,3 +10,5 @@ python -m pip wheel                     \
     --disable-pip-version-check         \
     -w "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}" \
     .
+
+ci/validate_wheel.sh "${RAPIDS_WHEEL_BLD_OUTPUT_DR}"
