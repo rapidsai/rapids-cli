ba0763c39771cdde01022d438f16646d57e80251
James Lamb <jaylamb20@gmail.com>
2026-01-30 14:46:12 -0600

    enforce PEP 639 metadata, use rapids copyright hook, validate wheels (#132)
    Proposes a batch of miscellaneous build / packaging / CI changes.
    
    ## Changes
    
    ### Adds size limit enforcement
    
    Contributes to https://github.com/rapidsai/build-planning/issues/219
    
    Adds some wheel validation used across most of RAPIDS:
    
    * package size is not too large (`pydistcheck`)
    * metadata is well-formatted (`twine`)
    
    ### Enforces PEP 639 license metadata in `pyproject.toml`
    
    Contributes to https://github.com/rapidsai/pre-commit-hooks/issues/95
    
    ### Switches to the RAPIDS hook for updating copyright comments
    
    RAPIDS maintains its own `pre-commit` hook for updating copyright headers, and this is used across most RAPIDS libraries.
    
    Benefits of replacing https://github.com/Lucas-C/pre-commit-hooks with this:
    
    * 1 less external dependency
    * write `SPDX-License-Identifier:` comments, which make sources friendly for scanners and SBOM generators
    * will update with the rest of RAPIDS if/when RAPIDS changes the way it expresses copyright comments
    
    ### Updates all `pre-commit` hooks
    
    ```shell
    pre-commit autoupdate
    ```
    
    That one blank newline added in a Python file was done by `black` 😅
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Jacob Tomlinson (https://github.com/jacobtomlinson)
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/rapids-cli/pull/132

--- rapids_cli/debug/debug.py
+++ rapids_cli/debug/debug.py
@@ -1,7 +1,7 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES.
-# All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 """This module contains the debug subcommand for the Rapids CLI."""
+
 import json
 import platform
 import subprocess
